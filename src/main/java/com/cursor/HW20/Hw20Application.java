f740dc17e68db42c456db9dacfa0de7f9ffe352d
maksim-protsenko <protsenko.maksim.88@gmail.com>
2021-03-04 23:28:09 +0200

    first commit

---
+++ src/main/java/com/cursor/HW20/Hw20Application.java
@@ -0,0 +1,14 @@
+package com.cursor.HW20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Hw20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw20Application.class, args);
+    }
+}
